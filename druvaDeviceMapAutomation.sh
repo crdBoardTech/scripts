25b2145737e91ea72b2135a671d6d11684e06eaa
crdBoardTech <102623834+crdBoardTech@users.noreply.github.com>
2022-07-08 14:28:31 -0400

    Update druvaDeviceMapAutomation.sh

--- druvaDeviceMapAutomation.sh
+++ druvaDeviceMapAutomation.sh
@@ -35,33 +35,137 @@ EmailAddress="$( curl -su ${apiUser}:${apiPass} -X GET "${JAMFURL}/JSSResource/c
 SerialNumber="$( curl -su ${apiUser}:${apiPass} -X GET "${JAMFURL}/JSSResource/computers/id/$id/subset/general" -H "accept: application/xml"| xmllint --format - | awk -F">|<" '/<serial_number/{print $3}')"
 ComputerName="$( curl -su ${apiUser}:${apiPass} -X GET "${JAMFURL}/JSSResource/computers/id/$id/subset/general" -H "accept: application/xml"| xmllint --format - | awk -F">|<" '/<name/{print $3}'| awk 'NR==1{print $1,$2}')"
 UserName="$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')"
+date="$( date -v-30d '+%Y-%m-%d')"
 
+echo $date
 ## Defining functions
 ###Install Homebrew and jq
 function installHomebrew {
-  installbrew=$(NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
-  installjq=$(/opt/homebrew/bin/brew install jq)
+#!/bin/sh
+# AutoBrew - Install Homebrew with root
+# Source: https://github.com/kennyb-222/AutoBrew/
+# Author: Kenny Botelho
+# Version: 1.2
+
+# Set environment variables
+HOME="$(mktemp -d)"
+export HOME
+export USER=root
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+BREW_INSTALL_LOG=$(mktemp)
+
+# Get current logged in user
+TargetUser=$(echo "show State:/Users/ConsoleUser" | \
+    scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+
+# Check if parameter passed to use pre-defined user
+if [ -n "$3" ]; then
+    # Supporting running the script in Jamf with no specialization via Self Service
+    TargetUser=$3
+elif [ -n "$1" ]; then
+    # Fallback case for the command line initiated method
+    TargetUser=$1
+fi
+
+# Ensure TargetUser isn't empty
+if [ -z "${TargetUser}" ]; then
+    /bin/echo "'TargetUser' is empty. You must specify a user!"
+    exit 1
+fi
+
+# Verify the TargetUser is valid
+if /usr/bin/dscl . -read "/Users/${TargetUser}" 2>&1 >/dev/null; then
+    /bin/echo "Validated ${TargetUser}"
+else
+    /bin/echo "Specified user \"${TargetUser}\" is invalid"
+    exit 1
+fi
+
+# Install Homebrew | strip out all interactive prompts
+/bin/bash -c "$(curl -fsSL \
+    https://raw.githubusercontent.com/Homebrew/install/master/install.sh | \
+    sed "s/abort \"Don't run this as root\!\"/\
+    echo \"WARNING: Running as root...\"/" | \
+    sed 's/  wait_for_user/  :/')" 2>&1 | tee "${BREW_INSTALL_LOG}"
+
+# Reset Homebrew permissions for target user
+brew_file_paths=$(sed '1,/==> This script will install:/d;/==> /,$d' \
+    "${BREW_INSTALL_LOG}")
+brew_dir_paths=$(sed '1,/==> The following new directories/d;/==> /,$d' \
+    "${BREW_INSTALL_LOG}")
+# Get the paths for the installed brew binary
+brew_bin=$(echo "${brew_file_paths}" | grep "/bin/brew")
+brew_bin_path=${brew_bin%/brew}
+# shellcheck disable=SC2086
+chown -R "${TargetUser}":admin ${brew_file_paths} ${brew_dir_paths}
+chgrp admin ${brew_bin_path}/
+chmod g+w ${brew_bin_path}
+
+# Unset home/user environment variables
+unset HOME
+unset USER
+
+# Finish up Homebrew install as target user
+su - "${TargetUser}" -c "${brew_bin} update --force"
+
+# Run cleanup before checking in with the doctor
+su - "${TargetUser}" -c "${brew_bin} cleanup"
+
+# Check for post-installation issues with "brew doctor"
+doctor_cmds=$(su - "${TargetUser}" -i -c "${brew_bin} doctor 2>&1 | grep 'mkdir\|chown\|chmod\|echo\|&&'")
+
+# Run "brew doctor" remediation commands
+if [ -n "${doctor_cmds}" ]; then
+    echo "\"brew doctor\" failed. Attempting to repair..."
+    while IFS= read -r line; do
+        echo "RUNNING: ${line}"
+        if [[ "${line}" == *sudo* ]]; then
+            # run command with variable substitution
+            cmd_modified=$(su - "${TargetUser}" -c "echo ${line}")
+            ${cmd_modified}
+        else
+            # Run cmd as TargetUser
+            su - "${TargetUser}" -c "${line}"
+        fi
+    done <<< "${doctor_cmds}"
+fi
+
+# Check Homebrew install status, check with the doctor status to see if everything looks good
+if su - "${TargetUser}" -i -c "${brew_bin} doctor"; then
+    echo 'Homebrew Installation Complete! Your system is ready to brew.'
+else
+    echo 'AutoBrew Installation Failed'
+    exit 1
+fi
 }
+
 ###Uninstall Homebrew
-function uninstallHomebrew {
-  uninstallbrew=$(NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)")
+function installjq {
+
+  if [[ $(uname -m) == 'arm64' ]]; then
+    su "${UserName}" -c "/opt/homebrew/bin/brew install jq"
+  else
+    su "${UserName}" -c "/usr/local/bin/brew install jq"
+  fi
 }
-###Get User id from Druva
-function DruvaUserID {
+###Get User id from Druva on m1 devices
+function DruvaUserIDm1 {
   Emailid="${UserName}"%40nols.edu""
   DruvaUserid="$(curl --location --request GET "https://apis.druva.com/insync/usermanagement/v1/users?emailID=${Emailid}" \
   --header "Accept: application/json" \
   --header "Authorization: Bearer $BearerToken" |\
   /opt/homebrew/bin/jq -r '.users[].userID'
   )"
+  echo "$DruvaUserid"
 }
-###Get devices associated with user
-function UserDevices {
+###Get devices associated with user  on m1 devices
+function UserDevicesm1 {
 #Pulls Device ID from most current Backup
-DruvaDeviceid="$(curl --location --request GET "https://apis.druva.com/insync/endpoints/v1/backups?lastSuccessful=true&userID=$DruvaUserid" \
+DruvaDeviceid="$(curl --location --request GET "https://apis.druva.com/insync/endpoints/v1/backups?lastSuccessful=true&userID=$DruvaUserid&minBackupStartTime=${date}T00:00:00Z" \
   --header "Accept: application/json" \
   --header "Authorization: Bearer $BearerToken" |\
   /opt/homebrew/bin/jq -r 'first(.backups[].deviceID)')"
+  echo "$DruvaDeviceid"
 
 #Gets Device Name from Most current backup
 LastBackupDeviceName="$(curl --location --request GET "https://apis.druva.com/insync/endpoints/v1/devices/$DruvaDeviceid" \
@@ -69,6 +173,34 @@ LastBackupDeviceName="$(curl --location --request GET "https://apis.druva.com/in
 --header "Authorization: Bearer $BearerToken" |\
 /opt/homebrew/bin/jq -r '.deviceName'
 )"
+echo "$LastBackupDeviceName"
+}
+###Get User id from Druva for intel
+function DruvaUserID_nonm1 {
+  Emailid="${UserName}"%40nols.edu""
+  DruvaUserid="$(curl --location --request GET "https://apis.druva.com/insync/usermanagement/v1/users?emailID=${Emailid}" \
+  --header "Accept: application/json" \
+  --header "Authorization: Bearer $BearerToken" |\
+  /usr/local/bin/jq -r '.users[].userID'
+  )"
+  echo "$DruvaUserid"
+}
+###Get devices associated with user for intel
+function UserDevices_nonm1 {
+#Pulls Device ID from most current Backup
+DruvaDeviceid="$(curl --location --request GET "https://apis.druva.com/insync/endpoints/v1/backups?lastSuccessful=true&userID=$DruvaUserid&minBackupStartTime=${30date}T00:00:00Z" \
+  --header "Accept: application/json" \
+  --header "Authorization: Bearer $BearerToken" |\
+  /usr/local/bin/jq -r 'first(.backups[].deviceID)')"
+  echo "$DruvaDeviceid"
+
+#Gets Device Name from Most current backup
+LastBackupDeviceName="$(curl --location --request GET "https://apis.druva.com/insync/endpoints/v1/devices/$DruvaDeviceid" \
+--header "Accept: application/json" \
+--header "Authorization: Bearer $BearerToken" |\
+//usr/local/bin/jq -r '.deviceName'
+)"
+echo "$LastBackupDeviceName"
 }
 ### Druva Device Map Api Call
 ####For Users with no available backups
@@ -100,16 +232,25 @@ curl -v --location --request POST "https://apis.druva.com/insync/endpoints/v1/de
     \"deviceIdentifierType\": \"serial-number\",
     \"deviceIdentifier\": \"$SerialNumber\",
     \"oldDeviceName\": \"$LastBackupDeviceName\",
-    \"restoreData\": \"DATA\"
+    \"restoreData\": \"ALL\"
 }"
 }
 
 installHomebrew
+installjq
+if [[ $(uname -m) == 'arm64' ]]; then
+  DruvaUserIDm1
+  UserDevicesm1
+else
+  DruvaUserID_nonm1
+  UserDevices_nonm1
+fi
+
 if [ $LastBackupDeviceName == "null"  ]; then
 DruvaMapNoRestore
 echo "Status: Computer is being backed up." >> "$DEP_NOTIFY_LOG"
 else
-  DruvaMapRestorce
+  DruvaMapRestore
   echo "Status: Device is being restored." >> "$DEP_NOTIFY_LOG"
 fi
 
